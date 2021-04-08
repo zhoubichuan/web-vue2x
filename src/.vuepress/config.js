ed860247c559e4c24460cb445f0acee95fcc147d
zhoubichuan <zhoubichuan@icloud.com>
2021-04-09 00:00:12 +0800

    master

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -63,7 +63,7 @@ module.exports = {
         nav: [{
             text: '基础知识',
             items: [{
-                text: '1.构建基础篇',
+                text: '1.前端工程化',
                 link: '/base/build/1.config'
               },
               {
@@ -75,7 +75,7 @@ module.exports = {
           {
             text: '高级知识',
             items: [{
-                text: '1.组件精讲',
+                text: '1.组件开发',
                 link: '/senior/componentElaboration/1.recursionAndDynamics'
               },
               {
