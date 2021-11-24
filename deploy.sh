44b9a0827d241a44f045dca6fccdf7f55bc1d0c7
zhoubichuan <zhoubichuan@icloud.com>
2021-11-24 23:34:28 +0800

    master

--- deploy.sh
+++ deploy.sh
@@ -7,21 +7,6 @@ git push github master
 # 2.打包
 npm run src:build
 
-cd dist
-# 3.发布到gh-pages分支
-git init
-git checkout --orphan gh-pages
-git add .
-git commit -m 'gh-pages'
-git remote add gitee https://gitee.com/zhoubichuan/web-vue.git
-git push -f gitee gh-pages
-
-# 4.返回初始目录
-cd -
-
-# 2.打包
-npm run src:build
-
 # 3.进入打包目录
 cd dist
 
@@ -34,4 +19,25 @@ git remote add github https://github.com/zhoubichuan/web-vue.git
 git push -f github gh-pages
 
 # 5.返回初始目录
+cd -
+
+# 1.发布到master分支
+git add -A
+git commit -m 'master'
+git pull gitee master
+git push gitee master
+
+# 2.打包
+npm run src:build
+
+cd dist
+# 3.发布到gh-pages分支
+git init
+git checkout --orphan gh-pages
+git add .
+git commit -m 'gh-pages'
+git remote add gitee https://gitee.com/zhoubichuan/web-vue.git
+git push -f gitee gh-pages
+
+# 4.返回初始目录
 cd -
