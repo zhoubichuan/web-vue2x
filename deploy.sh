6a2b03f5fe0f7080dff578c44b65654103fd26e6
zhoubichuan <zhoubichuan@icloud.com>
2021-11-24 23:36:50 +0800

    master

--- deploy.sh
+++ deploy.sh
@@ -1,25 +1,25 @@
-# 1.发布到master分支
-git add -A
-git commit -m 'master'
-git pull github master
-git push github master
+# # 1.发布到master分支
+# git add -A
+# git commit -m 'master'
+# git pull github master
+# git push github master
 
-# 2.打包
-npm run src:build
+# # 2.打包
+# npm run src:build
 
-# 3.进入打包目录
-cd dist
+# # 3.进入打包目录
+# cd dist
 
-# 4.发布到gh-pages分支
-git init
-git checkout --orphan gh-pages
-git add .
-git commit -m 'gh-pages'
-git remote add github https://github.com/zhoubichuan/web-vue.git
-git push -f github gh-pages
+# # 4.发布到gh-pages分支
+# git init
+# git checkout --orphan gh-pages
+# git add .
+# git commit -m 'gh-pages'
+# git remote add github https://github.com/zhoubichuan/web-vue.git
+# git push -f github gh-pages
 
-# 5.返回初始目录
-cd -
+# # 5.返回初始目录
+# cd -
 
 # 1.发布到master分支
 git add -A
