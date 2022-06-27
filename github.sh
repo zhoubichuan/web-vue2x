26cb9c531a5086e64390c397636ea2159874f5be
zhoubichuan <zhoubichuan@icloud.com>
2022-06-28 01:31:00 +0800

    master

--- github.sh
+++ github.sh
@@ -1,18 +1,4 @@
-# 自动化打包同时将打包后的代码发布到gh-pages分支
-
-# 1.打包
-npm run src:build
-
-# 2.进入打包目录
-cd dist
-
-# 3.发布到gh-pages分支
-git init
-git checkout --orphan gh-pages
-git add .
-git commit -m 'gh-pages'
-git remote add github https://github.com/zhoubichuan/web-vue.git
-git push -f github gh-pages
-
-# 4.返回初始目录
-cd -
+git add -A
+git commit -m 'master'
+git pull github master
+git push github master
