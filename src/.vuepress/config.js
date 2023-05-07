f9e75fad09efac7469c2861f02cde2b2b83871d8
周必川 <zhoubichuan@icloud.com>
2023-05-08 00:03:48 +0800

    update

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -18,7 +18,10 @@ module.exports = {
     // 名称：@vuepress/plugin-pwa 网页内容有更新的时候有刷新按钮。可以把网页保存到桌面，当一个app一样
     '@vuepress/pwa': {
       serviceWorker: true,
-      updatePopup: true
+      updatePopup: {
+        message: "有新的内容更新",
+        buttonText: "刷新"
+      }
     },
     // 名称：@vuepress/plugin-back-to-top 效果：文章看到下面的时候，点击一个图标会回到顶部
     '@vuepress/back-to-top': true,
