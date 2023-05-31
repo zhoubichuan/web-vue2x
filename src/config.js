14703e05e84989f40720168dc90ae98cfe626c1f
周必川 <zhoubichuan@icloud.com>
2023-05-31 23:52:53 +0800

    update

--- src/config.js
+++ src/config.js
@@ -2,15 +2,9 @@ import * as api from '@/api/index.js'
 import * as _ from 'lodash'
 import axios from 'axios'
 import service from '@/util/request.js'
-import echarts from 'echarts'
-import * as china from 'echarts/map/json/china.json'
-import * as world from 'echarts/map/json/world.json'
 
 export default {
   install (Vue) {
-    Vue.prototype.$echarts = echarts
-    Vue.prototype.$china = china.default
-    Vue.prototype.$world = world.default
     Vue.prototype._ = _
     // window._ = _
     // window.$env = process.env
