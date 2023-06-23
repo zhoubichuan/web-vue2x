3d0384a77200411e507470e930672b800b476ccb
周必川 <zhoubichuan@icloud.com>
2023-06-23 11:30:44 +0800

    update

--- src/ui.js
+++ src/ui.js
@@ -2,10 +2,10 @@
 // import 'static/css/reset.css'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-// import WebElementui from 'web-elementui'
+import WebElementui from 'web-elementui'
 export default {
   install (Vue) {
     Vue.use(ElementUI)
-    // Vue.use(WebElementui)
+    Vue.use(WebElementui)
   }
 }
