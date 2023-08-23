8f217c857c48f8dc344f40b24508c3915c1e897d
周必川 <zhoubichuan@icloud.com>
2023-08-23 21:51:21 +0800

    update

--- src/.vuepress/enhanceApp.js
+++ src/.vuepress/enhanceApp.js
@@ -1,12 +1,10 @@
 // 通过该文件，把需要用的组件进行全局的注册，因为在markdown中的文件不能使用import引入组件，必须要提前注册好全局的组件
-import xminder from "./xmind.vue";
 import config from "@/config";
 import ui from "@/ui";
 import directive from "@/directive";
 import store from "@/store";
 import i18n from "@/i18n";
 export default ({ Vue }) => {
-  Vue.component("xminder", xminder);
   Vue.use(directive);
   Vue.use(ui);
   Vue.use(config);
