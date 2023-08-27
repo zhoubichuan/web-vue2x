c210a99fe82cacb36f21f798246737a96fef4e4e
周必川 <zhoubichuan@icloud.com>
2023-08-27 23:45:58 +0800

    update

---
+++ src/.vuepress/components/VueButton.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="vue-button"><slot></slot></div>
+</template>
+<script>
+export default {
+  name: "VueButton",
+};
+</script>
+<style lang="scss" scoped>
+.vue-button {
+  display: inline-block;
+  padding: 10px 20px;
+  border: 1px solid gray;
+  border-radius: 4px;
+}
+</style>
