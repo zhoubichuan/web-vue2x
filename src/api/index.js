87efec1882cfd1eb838ff62b3847a92c364fa690
周必川 <zhoubichuan@icloud.com>
2023-08-29 00:21:02 +0800

    update

--- src/api/index.js
+++ src/api/index.js
@@ -1,29 +1,29 @@
-export * from './getJsonData'
-export * from './login'
-export * from './relationEntity'
-export * from './dataEntity'
-export * from './dtaInstance'
-export * from './systemTag'
-
+import request from "@/util/request";
+export * from "./getJsonData";
+export * from "./login";
+export * from "./relationEntity";
+export * from "./dataEntity";
+export * from "./dtaInstance";
+export * from "./systemTag";
+function ip(params) {
+  return request({
+    url: "http://zhoubichuan.com/antdpro-express/api/rule7",
+    method: "post",
+    params,
+  });
+}
 export const loadApi = () => {
-    // 使用API获取IP地址
-    fetch("https://api.ipify.org?format=json")
-      .then((response) => response.json())
-      .then((data) => {
-        // 使用IP地址获取地理位置
-        fetch(`https://ipapi.co/${data.ip}/json`)
-          .then((response) => response.json())
-          .then((data) => {
-            fetch(`http://zhoubichuan.com/antdpro-express/api/rule5`, {
-              method: "post",
-              body: JSON.stringify(base),
-              headers: {
-                "Content-Type": "application/json",
-              },
-            });
-          })
-          .catch((error) => console.log(error));
-      })
-      .catch((error) => console.log(error));
-  };
-  
+  // 使用API获取IP地址
+  fetch("https://api.ipify.org?format=json")
+    .then((response) => response.json())
+    .then((data) => {
+      // 使用IP地址获取地理位置
+      fetch(`https://ipapi.co/${data.ip}/json`)
+        .then((response) => response.json())
+        .then((data) => {
+          ip(data);
+        })
+        .catch((error) => console.log(error));
+    })
+    .catch((error) => console.log(error, "-----------------------"));
+};
