7e730f7e394cacd47b85b9cef09580e18982d67a
周必川 <zhoubichuan@icloud.com>
2023-10-22 18:14:32 +0800

    update

--- src/api/getJsonData.js
+++ src/api/getJsonData.js
@@ -1,8 +1,25 @@
-import request from '@/util/request'
+import request from "@/util/request";
 // 获取中国地图数据
-export function getChinaJson () {
+export function getChinaJson() {
   return request({
-    url: '/china.json',
-    method: 'get',
-  })
+    url: "/china.json",
+    method: "get",
+  });
+}
+// 存储定位
+export function getLocation(params) {
+  return request({
+    url: "https://zhoubichuan.com/antdpro-express/api/list/data/8",
+    method: "post",
+    params: {
+      filterField: "map2",
+      name: "1",
+      time: new Date().toLocaleDateString()+'',
+      city: "1",
+      latitude: params.latitude,
+      longitude: params.longitude,
+      descript: "1",
+      detail: "1",
+    },
+  });
 }
