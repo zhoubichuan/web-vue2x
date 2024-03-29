5ffa2767f3345294e6d105dd6f39405e5d04f570
zhoubichuan <zhoubichuan@icloud.com>
2024-03-29 23:55:05 +0800

    更新案例

--- components/component/component.vue
+++ components/component/component.vue
@@ -4,32 +4,33 @@
   </component>
 </template>
   <script>
-  export default{
-    name:'VueComponent',
-   props:{
-     to:{
-       type:String,
-       default:''
-     },
-     target:{
-       type:String,
-       default:"_self"
-     }
-   },
-   computed:{
-     tagName(){
-       return this.to === ''?'button':'a' // 动态渲染不同的标签
-     },
-     tagProps(){ // 如果是链接，把这些属性绑定到component上
-       let props = {}
-       if(this.to){
-         props = {
-           target:this.target,
-           href:this.to
-         }
-       }
-       return props
-     }
-   }
-  }
-  </script>
+export default {
+  name: "VueComponent",
+  props: {
+    to: {
+      type: String,
+      default: "",
+    },
+    target: {
+      type: String,
+      default: "_self",
+    },
+  },
+  computed: {
+    tagName() {
+      return this.to === "" ? "button" : "a"; // 动态渲染不同的标签
+    },
+    tagProps() {
+      // 如果是链接，把这些属性绑定到component上
+      let props = {};
+      if (this.to) {
+        props = {
+          target: this.target,
+          href: this.to,
+        };
+      }
+      return props;
+    },
+  },
+};
+</script>
--- components/menu/resub-menu.vue
+++ components/menu/resub-menu.vue
@@ -1,28 +1,28 @@
 <template>
-  <SubMenu>
+  <sub-menu>
     <template #title>
       {{ data.title }}
     </template>
     <div v-for="(child, index) in data.children" :key="index">
-      <MenuItem v-if="!child.children">{{ child.title }}</MenuItem>
-      <ReSub v-else :data="child"></ReSub>
+      <menu-item v-if="!child.children">{{ child.title }}</menu-item>
+      <vue-resub-menu v-else :data="child"></vue-resub-menu>
     </div>
-  </SubMenu>
+  </sub-menu>
 </template>
 <script>
 import SubMenu from "./sub-menu";
 import MenuItem from "./menu-item";
 export default {
   name: "VueResubMenu", //可以使用递归组件
+  components: {
+    SubMenu,
+    MenuItem,
+  },
   props: {
     data: {
       type: Object,
       default: () => ({}),
     },
-  },
-  components: {
-    SubMenu,
-    MenuItem,
   },
 };
 </script>
