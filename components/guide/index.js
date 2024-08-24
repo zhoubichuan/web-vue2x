a33c6d7622d5389ba0c6e137afbd35b7ea853911
zhoubichuan <zhoubichuan@icloud.com>
2024-08-24 13:54:34 +0000

    guide
    Signed-off-by: zhoubichuan <zhoubichuan@icloud.com>

--- components/guide/index.js
+++ components/guide/index.js
@@ -1,6 +1,8 @@
-import Guide from './src/vue-guide.vue'
-/* istanbul ignore next */
+import Guide from "./src/guide";
 Guide.install = function (Vue) {
-  Vue.component(Guide.name, Guide)
-}
-export default Guide
+    Vue.component(Guide.name, Guide);
+};
+export default Guide;
+// 1.支持svg复杂形状
+// 2.回调函数支持dom操作
+// 3.支持窗口大小变化
